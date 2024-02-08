777e4ff1666753e210c57b92ed51c838ecb873f5
NishitVAts <nishitvats@icloud.com>
2024-02-08 23:25:06 +0530

    Initial Commit

---
+++ E-Commerce App/E_Commerce_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_AppApp.swift
+//  E-Commerce App
+//
+//  Created by Nishit Vats on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
